8949ea2180f0991fbe9a6fc33e25357c7745edb0
Aakash Suresh <aakashbabus@gmail.com>
2024-05-16 21:44:21 +0530

    Basic Functionality added

---
+++ 01_Access_OpenAI_Model.py
@@ -0,0 +1,15 @@
+
+from openai import OpenAI
+
+client = OpenAI(api_key="Your OpenAI key , retain double quotes")
+
+# Execute the code block
+response = client.completions.create(
+model="davinci-002",
+prompt="malleshwaram is in the ",
+temperature=0.7,
+max_tokens=200
+)
+
+print(response.choices[0].text.strip())
+    
